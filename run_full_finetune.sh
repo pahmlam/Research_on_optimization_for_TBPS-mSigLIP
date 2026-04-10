030c2e7196a205fe8b773fa0e582952f911f919b
pahmlam <18phamtunglam@gmail.com>
2026-04-10 14:18:39 +0700

    add visualize

--- run_full_finetune.sh
+++ run_full_finetune.sh
@@ -4,6 +4,8 @@ uv run trainer.py -cn cir_msiglip \
     trainer.max_epochs=60 \
     trainer.accumulate_grad_batches=4 \
     ++trainer.precision=16-mixed \
+    trainer.gradient_clip_val=1.0 \
+    trainer.gradient_clip_algorithm=norm \
     \
     dataset.batch_size=24 \
     optimizer=cir_test \
