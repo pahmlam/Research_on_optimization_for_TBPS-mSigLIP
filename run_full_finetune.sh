311a152d6e293fc10d0f980444df36ade21c6a04
pahmlam <18phamtunglam@gmail.com>
2026-04-10 13:34:30 +0700

    add fft

--- run_full_finetune.sh
+++ run_full_finetune.sh
@@ -2,12 +2,10 @@
 
 uv run trainer.py -cn cir_msiglip \
     trainer.max_epochs=60 \
-    trainer.accumulate_grad_batches=1 \
+    trainer.accumulate_grad_batches=4 \
     ++trainer.precision=16-mixed \
     \
-    dataset.batch_size=64 \
-    'img_size_str="(384, 128)"' \
-    \
+    dataset.batch_size=24 \
     optimizer=cir_test \
     optimizer.param_groups.default.lr=1e-5 \
     optimizer.param_groups.backbone.lr=1e-5
